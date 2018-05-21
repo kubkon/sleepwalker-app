640c99d8648b600834bf3325a89da35815f73d52
Jakub Konka <kubkon@jakubkonka.com>
2018-05-21 23:35:12 +0200

    Fix mistake of using obj-c instead of swift

---
+++ sleepwalker-app WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  sleepwalker-app WatchKit Extension
+//
+//  Created by Jakub Konka on 21/05/2018.
+//  Copyright © 2018 Jakub Konka. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
