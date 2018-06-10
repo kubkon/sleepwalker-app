184156683e11a9f49bba7a8aa30a26e5b604fbd2
Jakub Konka <kubkon@jakubkonka.com>
2018-06-10 10:38:59 +0200

    Start redesigning the interfaces on WatchOS

--- sleepwalker-app WatchKit Extension/InterfaceController.swift
+++ sleepwalker-app WatchKit Extension/InterfaceController.swift
@@ -9,38 +9,40 @@
 import WatchKit
 import Foundation
 import WatchConnectivity
-import CoreMotion
-import HealthKit
+import os
 
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
     @IBOutlet weak var startRecordingButton: WKInterfaceButton!
-    
-    static let fs = 10 // 10 Hz
-    var sessionActivated = false
-    var accelActivated = false
     var session : WCSession?
-    var buffer : [Byte] = []
-    let motionManager = CMMotionManager()
-    let healthStore = HKHealthStore()
-    var workoutSession : HKWorkoutSession?
+    var sessionHandler: (() -> Void)?
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         // Configure interface objects here.
-        if (WCSession.isSupported() && !sessionActivated) {
-            session = WCSession.default
-            session!.delegate = self
-            session!.activate()
-            print("Activating the session!")
-        }
-        if motionManager.isAccelerometerAvailable {
-            motionManager.accelerometerUpdateInterval = 1 / Double(InterfaceController.fs)
-        }
+        sessionHandler = self.noWCConnectivity
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        if (WCSession.isSupported()) {
+            if (session == nil) {
+                os_log("Session was nil", log: OSLog.default, type: .debug)
+                // session not activated yet
+                session = WCSession.default
+                session?.delegate = self
+                session?.activate()
+            }
+        } else {
+            os_log("WatchConnectivity not supported", log: OSLog.default, type: .info)
+            // FIX notify user the app is incompatible with their devices
+            let action = WKAlertAction.init(title: "OK", style: WKAlertActionStyle.default, handler: {})
+            presentAlert(withTitle: "Update required",
+                         message: "Your AppleWatch seems to be incompatible with this app. Please update to the latest WatchOS.",
+                         preferredStyle: WKAlertControllerStyle.alert,
+                         actions: [action])
+        }
     }
     
     override func didDeactivate() {
@@ -49,46 +51,47 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
     
     @IBAction func startRecordingButtonTapped() {
-        print("startRecordingButton pressed!")
-        if !sessionActivated {
-            return
-        }
-        if !accelActivated {
-            workoutSession = try! HKWorkoutSession(configuration: HKWorkoutConfiguration.init())
-            healthStore.start(workoutSession!)
-            startRecordingButton.setTitle("Stop")
-            if motionManager.isAccelerometerAvailable {
-                motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { (data, error) in
-                    guard let data = data else {
-                        print("Couldn't unwrap accelerometer data: \(String(describing: error))")
-                        return
-                    }
-                    let accel = AccelReading(data.acceleration.x, data.acceleration.y, data.acceleration.z)
-                    let reading = Reading(Date.init().timeIntervalSince1970, accel)
-                    self.buffer += reading.toBytes()
-                    if (self.buffer.count == Reading.SIZE_BYTES * InterfaceController.fs * 10) { // 10secs, send the data
-                        let message = Data(bytes: self.buffer)
-                        WCSession.default.sendMessageData(message, replyHandler: nil, errorHandler: {(error) in
-                            print("Ooops, something went wrong: \(error)")
-                        })
-                        self.buffer.removeAll()
-                    }
-                })
-            }
-        }
-        else {
-            startRecordingButton.setTitle("Start")
-            motionManager.stopAccelerometerUpdates()
-            buffer.removeAll()
-            if let workoutSession = workoutSession {
-                healthStore.end(workoutSession)
-            }
-        }
-        accelActivated = !accelActivated
+        os_log("startRecordingButton pressed", log: OSLog.default, type: .debug)
+        if let f = sessionHandler { f() }
     }
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        print("Session activated!")
-        sessionActivated = true
+        if let error = error {
+            // FIX notify user that couldn't activate the session between the app and iOS counterpart
+            os_log("There was an error when trying to activate WCSession", log: OSLog.default, type: .error, "\(error.localizedDescription)")
+            self.session = nil
+            sessionHandler = noWCConnectivity
+            return
+        }
+        switch activationState {
+        case WCSessionActivationState.notActivated, WCSessionActivationState.inactive:
+            os_log("Session not activated/inactive", log: OSLog.default, type: .info)
+            self.session = nil
+            sessionHandler = noWCConnectivity
+        case WCSessionActivationState.activated:
+            fallthrough
+        default:
+            os_log("Session activated", log: OSLog.default, type: .info)
+            sessionHandler = startStopRecording
+        }
+    }
+    
+    func sessionReachabilityDidChange(_ session: WCSession) {
+        if (session.isReachable) { sessionHandler = startStopRecording }
+        else { sessionHandler = noWCConnectivity }
+    }
+    
+    func startStopRecording() {
+        os_log("Start recording...", log: OSLog.default, type: .info)
+    }
+    
+    func noWCConnectivity() {
+        os_log("No WC connectivity available", log: OSLog.default, type: .info)
+        // FIX inform user no WC connectivity with iOS counterpart
+        let action = WKAlertAction.init(title: "OK", style: WKAlertActionStyle.default, handler: {})
+        presentAlert(withTitle: "No connection",
+                     message: "Could not connect to iOS app. Make sure the SleepWalker app is running on your iPhone.",
+                     preferredStyle: WKAlertControllerStyle.alert,
+                     actions: [action])
     }
 }
